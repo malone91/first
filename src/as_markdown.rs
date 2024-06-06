c18727066d70e71b9bbb9b2a57986cf4355def1b
malong5 <malong5@xiaomi.com>
2024-06-06 11:28:51 +0800

    lock

--- src/as_markdown.rs
+++ src/as_markdown.rs
@@ -1,8 +1,21 @@
+use std::env;
+
 fn main() {
-    let url = "https://www.rust-lang.org/";
+    // let url = "https://www.rust-lang.org/";  cargo run --bin as_markdown -- https:www.baidu.com
+    // let args: Vec<String> = env::args().collect();
+    // let url = &args[1];
+
+    //与上面的都可以从命令中获取参数
+    let url = env::args().nth(1).unwrap();
 
     println!("fetching url {}", url);
     let body = reqwest::blocking::get(url).unwrap().text().unwrap();
 
     println!("response body html {}", body);
+
+    // target\debug\as_markdown.exe
+    // https:www.baidu.com
+    for arg in std::env::args() {
+        println!("{}", arg);
+    }
 }
